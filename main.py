0eb15e094d0298d585f458b9c96e5edabfc5158e
Leonid <105441782+LeoMosIg@users.noreply.github.com>
2022-07-28 22:15:06 +0300

    Дописан основной код

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from utils import get_candidates, format_candidates
+from utils import get_candidates, format_candidates, get_candidate_by_id, get_candidates_by_skill
 
 app = Flask(__name__)
 
@@ -12,14 +12,22 @@ def main():
     return format_candidates(candidates_list)
 
 
-@app.route('/candidates/<candidate_id>')
+@app.route('/candidates/<int:candidate_id>')
 def page_candidate(candidate_id):
-    pass
+    candidates_list = get_candidates('candidates.json')
+
+    candidate = get_candidate_by_id(candidates_list, candidate_id)
+
+    result = f'<img src="{candidate["picture"]}">'
+
+    return result + format_candidates([candidate])
 
 
-@app.route('/skills/<skill_id>')
-def skills(skill_id):
-    pass
+@app.route('/skills/<skill>')
+def skills(skill):
+    candidates_list = get_candidates('candidates.json')
+
+    return format_candidates(get_candidates_by_skill(candidates_list, skill))
 
 
 app.run()
--- utils.py
+++ utils.py
@@ -18,3 +18,21 @@ def format_candidates(candidates_list):
 
     result += '</pre>'
     return result
+
+
+def get_candidate_by_id(candidates_list, candidate_id):
+    for candidate in candidates_list:
+        if candidate['id'] == candidate_id:
+            return candidate
+
+
+def get_candidates_by_skill(candidates_list, candidate_skill):
+    result = []
+
+    for candidate in candidates_list:
+        candidate_skills = candidate['skills'].lower().split(', ')
+
+        if candidate_skill.lower() in candidate_skills:
+            result.append(candidate)
+
+    return result
